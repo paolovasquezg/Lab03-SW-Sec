cf22d65e2e3b5a924ea96900f09dd4d7c1d726ea
Paolo Vásquez <83627663+paolovasquezg@users.noreply.github.com>
2025-10-20 18:19:42 -0500

    vulnapp working

---
+++ vulnapp/main.py
@@ -0,0 +1,46 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from db import init_db, engine
+import subprocess
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan, title="Vulnapp")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+
+def testing():
+    return "Testing Vulnapp"
+
+@app.get("/ping")
+def ping(host: str):
+    command = f"ping -c 1 {host}"
+    output = subprocess.getoutput(command)
+    return {"command": command, "output": output}
+
+
+@app.get("/user")
+def user(username: str):
+    
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    cur.execute(query)
+
+    cols = [desc[0] for desc in cur.description]
+    rows = [dict(zip(cols, r)) for r in cur.fetchall()]
+    return {"query": query, "rows": rows}
